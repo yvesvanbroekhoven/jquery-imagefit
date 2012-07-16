39c9405c7f2028824b100236acda2adf09dfb603
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2012-07-16 19:24:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jquery-imagefit
+
+TODO: write documentation
---
+++ jquery-imagefit.js
@@ -0,0 +1,11 @@
+/*
+ * jquery-imagefit
+ *
+ * Created at: 2012-07-16 19:24:45 +0200
+ * Author: Steven Vandevelde & Yves Van Broekhoven
+ * Version: 0.0.0
+ */
+
+(function() {
+
+}(jQuery));
